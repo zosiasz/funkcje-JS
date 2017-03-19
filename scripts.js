15bf0ca2e9ee9409cf661a9fcd02817d9373d590
zosia <zofiaszczech1@gmail.com>
2017-03-19 12:58:03 +0100

    push

---
+++ scripts.js
@@ -0,0 +1,21 @@
+var a,
+	h;
+
+
+	function getTriangleArea(a, h) {
+
+	if (a,h <= 0) {
+		alert('Nieprawidłowe dane')
+		}
+
+	else { (a,h > 0)  
+		return a*h/2
+
+		}
+	}
+
+console.log( getTriangleArea(10,6) )
+
+var triangle1Area = getTriangleArea(10, 15);
+var	triangle2Area = getTriangleArea(10, 20);
+var	triangle3Area = getTriangleArea(10, 34);
